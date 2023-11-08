97b87c5915f8d0dab6725e1cf07153b0334bc16b
Chrismiweb <chrismibiteso@gmail.com>
2023-11-08 15:45:22 -0800

    added the component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './component/Navbar';
+import CartContainer from './component/CartContainer';
+import cartItems from './cart-item';
+import {createStore} from 'redux';
+import reducer from './reducer';
+// store- its used to store data in redux
+//  reducer - its a function used to update store
+// action -  its what happens or what updated 
+// state - old state/state before update
+// react-redux - provider - wraps app, connect - used in components
+import { Provider } from 'react-redux';
+
+// initial store
+const initialStore = {
+  cart: cartItems,
+  total: 105,
+  amount: 5
+};
+
+const store = createStore(reducer, initialStore);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Navbar />
+      <CartContainer  />
+    </Provider>
   );
 }
 
---
+++ src/cart-item.js
@@ -0,0 +1,30 @@
+import pro from '../src/images/11pro.png'
+import iphone11 from '../src/images/iphone11.png'
+import promax from '../src/images/promax.png'
+
+
+
+export default [
+    {
+      id: 1,
+      title: "Iphone 11",
+      price: 599.99,
+      img: iphone11,
+      amount: 1
+    },
+    {
+      id: 2,
+      title: "Iphone 11 pro ",
+      price: 499.99,
+      img: pro,
+      amount: 1
+    },
+    {
+      id: 3,
+      title: "Iphone 11 pro max",
+      price: 699.99,
+      img: promax ,
+      amount: 1
+    }
+  ];
+  
